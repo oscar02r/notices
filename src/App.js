c20b23b020bc15842feacec415a6c78c481add35
oscar02r <oscary.ramirez@gmail.com>
2020-05-03 02:49:24 +0200

    Getting notices from api in app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [ categoria, guardarCategoria ] = useState('');
+  const [ noticias, guardarNoticias   ] = useState([]);
+
+  useEffect(() => {
+    if(categoria.trim() === '') return;
+
+   const consultarApi =  async () => {
+         const url =`http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=08e1c1de7aed42ca83cf08e582ccd988`;
+
+         const respuesta = await fetch(url);
+         const noticias =  await respuesta.json();
+         guardarNoticias(noticias.articles);
+   }
+   consultarApi();
+  }, [categoria]);
+
   return (
     <>
     <Header
@@ -10,7 +27,9 @@ function App() {
     />
       <div className="container white">
         
-       <Formulario/>
+       <Formulario
+          guardarCategoria= {guardarCategoria}
+       />
       </div>
     </>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,37 @@
 import React from "react";
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
+
+  const OPCIONES = [
+      { value:'general', label:'General'},
+      { value:'business', label:'Negocios'},
+      { value:'entertaiment', label:'Entretenimiento'},
+      { value:'health', label:'Salud'},
+      { value:'science', label:'Ciencia'},
+      { value:'sports', label:'Deportes'},
+      { value:'technology', label:'Tecnología'},
+  ];
+  const [categoria, SelectNoticias] = useSelect('technology',OPCIONES);
+
+  const buscarNoticias = e => {
+        e.preventDefault();
+        guardarCategoria(categoria);
+  }
   return (
       <div className={`${styles.buscador} row`}>
-
+        
        <div className="col s12 m8 offset-m2">
-        <form>
+        <form
+          onSubmit={buscarNoticias}
+        >
           <h2 className={styles.heading}>Encuntra Noticias por Categoria</h2>
+          <SelectNoticias
+
+          />
         <div className="input-field col s12">
+          
           <input type="submit" 
             className={`${styles.btn_block} btn-large amber darken-2`} 
             value="Buscar"
